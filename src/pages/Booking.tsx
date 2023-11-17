d349e47fb9c3a0fb5c89d1b87a7a052e0033c749
pastelite <kana.bhong@gmail.com>
2023-11-17 19:54:04 +0700

    Refactor equipment reservation and fix time validation

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react"
+import { useMemo, useState } from "react"
 import { useParams } from "react-router-dom"
 import { equipmentsStore } from "../stores"
 import courtsList, { CourtInfo } from "../courtsList"
@@ -8,49 +8,20 @@ import "./Booking.scss"
 import JamClock from "../assets/icons/jam-clock.svg?react"
 import JamCoin from "../assets/icons/jam-coin.svg?react"
 
-export default function Booking() {
-  let courtId = Number.parseInt(useParams().id ?? '0')
-  let courtInfo = useMemo(() => {
-    return courtsList.find((court) => court.id === courtId)
-  }, [])
+type StringNumberDict = { [key: string]: number }
 
-  let [{ startTime, endTime }, setBookingTime] = useState({
-    startTime: 0,
-    endTime: 0
-  })
-  let [formEquipments, setFormEquipments] = useState({
-    badmintonRack: 0,
-    shuttlecock: 0,
-  })
-  let [dbEquipments, setDbEquipments] = equipmentsStore((state) => {
-    let { setEquipments, ...equipments } = state
-    return [equipments, setEquipments]
-  })
+interface BookingEquipmentsProps {
+  formEquipment: StringNumberDict
+  setFormEquipment: React.Dispatch<React.SetStateAction<StringNumberDict>>
+}
 
-  let [totalTime, setTotalTime] = useState(0)
-
-  let [errorText, setErrorText] = useState('')
-
-  function handleChangeStart(e: React.ChangeEvent<HTMLInputElement>) {
-    let { value } = e.target
-    let time = convertTimeToInt(value)
-    setBookingTime((prev) => ({ ...prev, startTime: time }))
-
-    checkTimeValue(time, endTime)
-  }
-
-  function handleChangeEnd(e: React.ChangeEvent<HTMLInputElement>) {
-    let { value } = e.target
-    let time = convertTimeToInt(value)
-    setBookingTime((prev) => ({ ...prev, endTime: time }))
-
-    checkTimeValue(startTime, time)
-  }
+function BookingEquipments({ formEquipment, setFormEquipment }: BookingEquipmentsProps) {
+  let dbEquipments = equipmentsStore()
 
   function handleEquipmentChange(e: React.ChangeEvent<HTMLInputElement>) {
     let { name, value: valueString } = e.target
     let value = Number.parseInt(valueString)
-    setFormEquipments((prev) => ({ ...prev, [name]: value }))
+    setFormEquipment((prev) => ({ ...prev, [name]: value }))
 
     // @ts-ignore
     let dbEquipmentsNumber: number = dbEquipments[name];
@@ -65,70 +36,59 @@ export default function Booking() {
     }
   }
 
-  function checkTimeValue(startTime: number, endTime: number) {
-    console.log(startTime, endTime)
+  return (<p>
+    <h3>Equipment Reservation</h3>
+    <div className="equipment-container">
+      <div>
+        <h4>Badminton Rack</h4>
+        <p>
+          Available: {dbEquipments.badmintonRack - formEquipment.badmintonRack}<br />
+          Price: 5฿/hr
+        </p>
+        <input type="number" defaultValue={0} name="badmintonRack" onChange={handleEquipmentChange}></input>
+      </div>
+      <div>
+        <div>Shuttlecock</div>
+        <p>
+          Available: {dbEquipments.shuttlecock - formEquipment.shuttlecock}<br />
+          Price: 1฿/hr
+        </p>
+        <input type="number" defaultValue={0} name="shuttlecock" onChange={handleEquipmentChange}></input>
+      </div>
+    </div>
+  </p>);
+}
 
-    if (startTime > endTime) {
-      setErrorText('Error: End time must be after start time')
-    } else if (startTime < (courtInfo?.businessHour[0] || 0) * 100) {
-      setErrorText('Error: Start time must be after openning which is ' + (courtInfo?.businessHour[0] || 0) + ':00')
-    } else if (endTime > (courtInfo?.businessHour[1] || 0) * 100) {
-      setErrorText('Error: End time must be before closing which is ' + (courtInfo?.businessHour[1] || 0) + ':00')
-    } else {
-      setErrorText('')
-    }
-  }
 
-  function calculateTime(startTime: number, endTime: number) {
-    let minutes = endTime % 100 - startTime % 100
-    let hours = Math.floor((endTime - startTime) / 100)
+export default function Booking() {
+  // Get court info from url
+  let courtId = Number.parseInt(useParams().id ?? '0')
+  let courtInfo = useMemo(() => {
+    return courtsList.find((court) => court.id === courtId)
+  }, [])
 
-    if (minutes < 0) {
-      minutes += 60
-      hours -= 1
-    }
-
-    return [hours, minutes]
-  }
-
+  // Setup shared state
+  let [totalTime, setTotalTime] = useState(0)
+  let [formEquipments, setFormEquipments] = useState<StringNumberDict>({
+    badmintonRack: 0,
+    shuttlecock: 0,
+  })
+ 
   // calculate price
-  let hourlyPrice = ((courtInfo?.bookingCost || 0) + formEquipments.badmintonRack * 5 + formEquipments.shuttlecock * 1)
+  let bookingPrice = (courtInfo?.bookingCost || 0) 
+  let equipmentPrice = formEquipments.badmintonRack * 5 + formEquipments.shuttlecock * 1
+  let hourlyPrice = bookingPrice + equipmentPrice
   let price = hourlyPrice / 60 * totalTime
 
   return <div id="booking-page">
     <h1>Booking</h1>
     <BookingTime courtInfo={courtInfo} setTotalTime={setTotalTime} />
-    <p>
-      <h3>Equipment Reservation</h3>
-      <div className="equipment-container">
-        <div>
-          <h4>Badminton Rack</h4>
-          <p>
-            Available: {dbEquipments.badmintonRack - formEquipments.badmintonRack}<br />
-            Price: 5฿/hr
-          </p>
-          <input type="number" defaultValue={0} name="badmintonRack" onChange={handleEquipmentChange}></input>
-        </div>
-        <div>
-          <div>Shuttlecock</div>
-          <p>
-            Available: {dbEquipments.shuttlecock - formEquipments.shuttlecock}<br />
-            Price: 1฿/hr
-          </p>
-          <input type="number" defaultValue={0} name="shuttlecock" onChange={handleEquipmentChange}></input>
-        </div>
-      </div>
-    </p>
+    <BookingEquipments formEquipment={formEquipments} setFormEquipment={setFormEquipments}/>
     <p>
       <JamCoin fill='white' height='1em' />
       Final Price: {price.toFixed(2)}฿ ({hourlyPrice}฿/hr)
     </p>
   </div>
-}
-
-function convertTimeToInt(time: string) {
-  let [hour, minute] = time.split(':')
-  return Number.parseInt(hour) * 100 + Number.parseInt(minute)
 }
 
 
@@ -176,7 +136,7 @@ function BookingTime({ courtInfo, setTotalTime }: BookingTimeProps) {
   }
 
   // only set total time when the form changes to prevent infinite loop
-  let totalTime = useMemo(()=> {
+  let totalTime = useMemo(() => {
     let startTime = dayjs(formTime.startTime, 'HH:mm')
     let endTime = dayjs(formTime.endTime, 'HH:mm')
     let total = endTime.diff(startTime, 'minute')
